dba135a77429fafc6e3ee292d606d1b424d2767e
vythili6 <yo.vythilingum@mail.utoronto.ca>
2025-08-16 16:00:03 -0400

    linked the backend to the frontend using Axios

--- backend/api/views.py
+++ backend/api/views.py
@@ -12,4 +12,37 @@ class CountryViewSet(viewsets.ViewSet):
 
     def list(self, request):
         queryset = Country.objects.all()
+        serializer = self.serializer_class(queryset, many=True)
+        return Response(serializer.data)
+    
+
+class LeagueViewSet(viewsets.ViewSet):
+    permission_classes = [permissions.AllowAny]
+    queryset = League.objects.all()
+    serializer_class = LeagueSerializer
+
+    def list(self, request):
+        queryset = League.objects.all()
+        serializer = self.serializer_class(queryset, many=True)
+        return Response(serializer.data)
         
+
+class CharacteristicViewSet(viewsets.ViewSet):
+    permission_classes = [permissions.AllowAny]
+    queryset = Characteristic.objects.all()
+    serializer_class = CharacteristicSerializer
+
+    def list(self, request):
+        queryset = Characteristic.objects.all()
+        serializer = self.serializer_class(queryset, many=True)
+        return Response(serializer.data)
+    
+class FootballClubsViewSet(viewsets.ViewSet):
+    permission_classes = [permissions.AllowAny]
+    queryset = FootballClubs.objects.all()
+    serializer_class = FootballClubSerializer
+
+    def list(self, request):
+        queryset = FootballClubs.objects.all()
+        serializer = self.serializer_class(queryset, many=True)
+        return Response(serializer.data)
--- frontend/src/components/Create.jsx
+++ frontend/src/components/Create.jsx
@@ -1,9 +1,37 @@
-import React from 'react';
+import {React, useState, useEffect} from 'react';
+import AxiosInstance from './Axios';
+import {Box} from '@mui/material';
 
 const Create = () => {
+    const [country, setCountry] = useState([]);
+    const [league, setLeague] = useState([]);
+    const [characteristic, setCharacteristic] = useState([]);    
+
+    console.log("Country", country);
+    console.log("league", league);
+    console.log("characteristic", characteristic);
+
+    const GetData = () => {
+        AxiosInstance.get(`country/`).then((res) => {
+            setCountry(res.data);
+        })
+
+        AxiosInstance.get(`league/`).then((res) => {
+            setLeague(res.data);
+        })
+
+        AxiosInstance.get(`characteristic/`).then((res) => {
+            setCharacteristic(res.data);
+        })
+    }
+
+    useEffect(() => {
+        GetData();
+    }, [])
+
     return (
         <div>
-            <h1>This is the Create Page</h1>
+            <Box></Box>
         </div>
     )
 }
